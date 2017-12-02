f03f6ceeb8d3d69038c59f03046229460d511b75
weichunren <541231331@qq.com>
2017-12-02 13:52:58 +0800

    del space

--- function.php
+++ function.php
@@ -44,27 +44,27 @@ function my_curl($url, $fields){
 * @return array
 */
 function img_curl($imgpath, $hots, $port, $path = ''){
-    $data = file_get_contents($imgpath);  
-    $http_entity_body = $data;  
-    $http_entity_type = 'application/x-www-form-urlencoded';  
-    $http_entity_length = strlen($http_entity_body);  
-    $host = '127.0.0.1';  
-    $port = 88;  
-    $path = '';  
-    $fp = fsockopen($host, $port, $error_no, $error_desc, 30);  
-    if ($fp) {  
-      fputs($fp, "POST {$path} HTTP/1.1\r\n");  
-      fputs($fp, "Host: {$host}\r\n");  
-      fputs($fp, "Content-Type: {$http_entity_type}\r\n");  
-      fputs($fp, "Content-Length: {$http_entity_length}\r\n");  
-      fputs($fp, "Connection: close\r\n\r\n");  
-      fputs($fp, $http_entity_body . "\r\n\r\n");  
+    $data = file_get_contents($imgpath);
+    $http_entity_body = $data;
+    $http_entity_type = 'application/x-www-form-urlencoded';
+    $http_entity_length = strlen($http_entity_body);
+    $host = '127.0.0.1';
+    $port = 88;
+    $path = '';
+    $fp = fsockopen($host, $port, $error_no, $error_desc, 30);
+    if ($fp) {
+      fputs($fp, "POST {$path} HTTP/1.1\r\n");
+      fputs($fp, "Host: {$host}\r\n");
+      fputs($fp, "Content-Type: {$http_entity_type}\r\n");
+      fputs($fp, "Content-Length: {$http_entity_length}\r\n");
+      fputs($fp, "Connection: close\r\n\r\n");
+      fputs($fp, $http_entity_body . "\r\n\r\n");
        
-      while (!feof($fp)) {  
-        $d .= fgets($fp, 4096);  
-      }  
-      fclose($fp);  
-      return $d;  
+      while (!feof($fp)) {
+        $d .= fgets($fp, 4096);
+      }
+      fclose($fp);
+      return $d;
     }  
 }
 
